9c8d479f91126f1fa75b2eba96c277454298d655
wojteksuj <s31087@pjwstk.edu.pl>
2025-03-19 10:38:01 +0100

    Added PersonalComputer class

--- APBD/PersonalComputer.cs
+++ APBD/PersonalComputer.cs
@@ -2,6 +2,15 @@ namespace APBD;
 
 public class PersonalComputer : Device
 {
+    String system = null;
+
+    public PersonalComputer(int id, string name, bool isTurnedOn, String system)
+    {
+        this.id = id;
+        this.Name = name;
+        this.IsTurnedOn = isTurnedOn;
+        this.system = system;
+    }
     
     
     
