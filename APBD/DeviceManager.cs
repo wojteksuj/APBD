4c568da64f56db7e39a3ccd78ca03cfefc65ba8a
wojteksuj <s31087@pjwstk.edu.pl>
2025-03-19 17:29:03 +0100

    Inserting and updating device manager logic

--- APBD/DeviceManager.cs
+++ APBD/DeviceManager.cs
@@ -12,16 +12,60 @@ public class DeviceManager
             {
                 string[] line;
                 line = ln.Split(',');
-                if (line[0].StartsWith("SW"))
+                if (line[0].StartsWith("SW-"))
                 {
-                    
+                    char lastChar = line[0][^1];
+                    if (int.TryParse(lastChar.ToString(), out int result))
+                    {
+                        int id = result;
+                        string name = line[1];
+                        bool isTurnedOn = bool.Parse(line[2]);
+                        int battery = int.Parse(line[3].Replace("%",""));
+                        Device newSw = new Smartwatch(id, name, isTurnedOn, battery);
+                        if(devices.Count < 15) devices.Add(newSw);
+                        else throw new Exception("Too many devices");
+                    }
+                    else
+                    {
+                        throw new Exception("Wrong input format");
+                    }
                 }
-                else if (line[0].StartsWith("P"))
+                else if (line[0].StartsWith("P-"))
                 {
-                    
+                    char lastChar = line[0][^1];
+                    if (int.TryParse(lastChar.ToString(), out int result))
+                    {
+                        int id = result;
+                        string name = line[1];
+                        bool isTurnedOn = bool.Parse(line[2]);
+                        string system = null;
+                        if (line.Length > 3) system = line[3];
+                        Device newPc = new PersonalComputer(id, name, isTurnedOn, system);
+                        if(devices.Count < 15) devices.Add(newPc);
+                        else throw new Exception("Too many devices");
+                    }
+                    else
+                    {
+                        throw new Exception("Wrong input format");
+                    }
                 }
-                else if (line[0].StartsWith("ED"))
+                else if (line[0].StartsWith("ED-"))
                 {
+                    char lastChar = line[0][^1];
+                    if (int.TryParse(lastChar.ToString(), out int result))
+                    {
+                        int id = result;
+                        string name = line[1];
+                        string ipAddress = line[2];
+                        string networkName = line[3];
+                        Device newEd = new EmbeddedDevices(id, name, false, ipAddress, networkName);
+                        if(devices.Count < 15) devices.Add(newEd);
+                        else throw new Exception("Too many devices");
+                    }
+                    else
+                    {
+                        throw new Exception("Wrong input format");
+                    }
                     
                 }
                 else continue;
